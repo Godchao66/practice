526cd15bb958daea9fdefc86c6516ec77de046cb
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2021-12-06 19:44:45 +0800

    Add files via upload

---
+++ pta52/pta52/pta52.cpp
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	char arr[80] ;
+//	gets_s(arr);
+//	int i = 0;
+//	/*while (arr[i] != '\0')
+//	{
+//		printf("%c", arr[i]);
+//		i++;
+//	}
+//	*/
+//
+//	return 0;
+//}
+int main()
+{
+	//printf("%d", 'a');
+	char arr[2];
+	gets_s(arr);
+	printf("%d", arr[0]);
+
+}
