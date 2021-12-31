c9f8f07aa28a84614e4fb48c572be56c4c2042c9
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2021-12-31 14:13:45 +0800

    Add files via upload

---
+++ pta56/pta56/简单阶乘计算.cpp
@@ -0,0 +1,32 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int Factorial(const int N);
+
+int main()
+{
+    int N, NF;
+
+    scanf("%d", &N);
+    NF = Factorial(N);
+    if (NF)  printf("%d! = %d\n", N, NF);
+    else printf("Invalid input\n");
+
+    return 0;
+}
+
+
+/* 你的代码将被嵌在这里 */
+int Factorial(const int N)
+{
+	if (N < 0) {
+		return 0;
+	}
+	else if (N == 1 || N == 0)
+	{
+		return 1;
+	}
+	else {
+		return N * Factorial(N - 1);
+	}
+}
