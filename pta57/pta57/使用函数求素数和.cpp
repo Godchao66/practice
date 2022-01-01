fed2dbbbc2f1e4ce554fa98fabf5d9cf2b76c2dd
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2022-01-01 09:26:54 +0800

    Add files via upload

---
+++ pta57/pta57/使用函数求素数和.cpp
@@ -0,0 +1,59 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int prime(int p);
+int PrimeSum(int m, int n);
+
+int main()
+{
+    int m, n, p;
+
+    scanf("%d %d", &m, &n);
+    printf("Sum of ( ");
+    for (p = m; p <= n; p++) {
+        if (prime(p) != 0)
+            printf("%d ", p);
+    }
+    printf(") = %d\n", PrimeSum(m, n));
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+int prime(int p)
+{
+	int i, j;
+	if (p <= 1) {
+		return 0;
+	}
+	else if (p == 2)
+	{
+		return 1;
+	}
+	else {
+
+		for (i = 2; i < p; i++)
+		{
+			if (p % i == 0)
+				break;
+		}
+		if (i == p) {
+			return 1;
+		}
+		else return 0;
+	}
+}
+int PrimeSum(int m, int n)
+{
+	int i = m, sum = 0;
+	for (; i <= n; i++)
+	{
+		if (prime(i) == 1) {
+			sum += i;
+		}
+	}
+	return sum;
+
+
+}
