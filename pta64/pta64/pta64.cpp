727ac1063ecebdf00243bd46a2949958be410c91
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2022-01-09 15:29:03 +0800

    Add files via upload

---
+++ pta64/pta64/pta64.cpp
@@ -0,0 +1,45 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAXN 10
+
+void sort(int a[], int n);
+
+int main()
+{
+    int i, n;
+    int a[MAXN];
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    sort(a, n);
+
+    printf("After sorted the array is:");
+    for (i = 0; i < n; i++)
+        printf(" %d", a[i]);
+    printf("\n");
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+void sort(int a[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+
+
+}
